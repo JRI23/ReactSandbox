f3c60013a180d4ef1f8142ea1d788e8fdc5cdb00
JRI23 <joraijayden@outlook.com>
2021-10-13 11:51:01 +0200

    React sandbox routing

--- src/index.js
+++ src/index.js
@@ -1,17 +1,40 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import CommentDetail from './CommentDetail';
+import { Route, BrowserRouter as Router, Switch, Link } from "react-router-dom";
+import Home from "./Pages/Home";
+import About from "./Pages/About";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+const App = () => {
+    return (
+        <Router>
+
+            <nav>
+                <ul>
+                    <li>
+                        <Link to="/">Home</Link>
+                    </li>
+                    <li>
+                        <Link to="/about">About</Link>
+                    </li>
+                </ul>
+            </nav>
+
+            <div className="ui container comments">
+                <CommentDetail />
+                <CommentDetail />
+                <CommentDetail />
+            </div>
+
+            <Switch>
+
+                <Route path="/about" component={About} />
+                <Route path="/" component={Home} />
+
+            </Switch>
+        </Router>
+    );
+};
+
+ReactDOM.render(<App />, document.querySelector('#root'))
