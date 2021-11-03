161f092ea5e760df4be85385b7870a6af717c3c5
JRI23 <joraijayden@outlook.com>
2021-11-03 16:57:25 +0100

    Comment Cards

--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import CommentDetail from './CommentDetail';
-import { Route, BrowserRouter as Router, Switch, Link } from "react-router-dom";
+import {Route, BrowserRouter as Router, Switch, Link} from "react-router-dom";
 import Home from "./Pages/Home";
 import About from "./Pages/About";
+import faker from 'faker';
+import ApprovalCard from './ApprovalCard';
 
 
 const App = () => {
@@ -22,19 +24,26 @@ const App = () => {
             </nav>
 
             <div className="ui container comments">
-                <CommentDetail />
-                <CommentDetail />
-                <CommentDetail />
+                <ApprovalCard>
+                    <CommentDetail author="Jake" timeAgo="Today at 19:25" content="Hello"
+                                   avatar={faker.image.avatar()}/>
+                </ApprovalCard>
+
+                <ApprovalCard>
+                    <CommentDetail author="Hikmet" timeAgo="Yesterday at 18:17" content="Wazzup"
+                                   avatar={faker.image.avatar()}/>
+                </ApprovalCard>
+                <ApprovalCard>
+                    <CommentDetail author="Savage" timeAgo="Today at 13:56" content="React is legendary"
+                                   avatar={faker.image.avatar()}/>
+                </ApprovalCard>
             </div>
-
             <Switch>
-
-                <Route path="/about" component={About} />
-                <Route path="/" component={Home} />
-
+                <Route path="/about" component={About}/>
+                <Route path="/" component={Home}/>
             </Switch>
         </Router>
     );
 };
 
-ReactDOM.render(<App />, document.querySelector('#root'))
+ReactDOM.render(<App/>, document.querySelector('#root'))
