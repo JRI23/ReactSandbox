026aa88e99e4abf52f6181977e41327b39225e3d
JRI23 <joraijayden@outlook.com>
2021-11-09 15:11:12 +0100

    Page fetches latitude

--- src/containers/Seasons/Seasons.js
+++ src/containers/Seasons/Seasons.js
@@ -1,9 +1,39 @@
 import React from "react";
 
-export default function Seasons() {
-    return (
-        <div>
-            <h1>Seasonsss</h1>
-        </div>
-    )
+
+export default class Seasons extends React.Component {
+    constructor(props) {
+        super(props);
+
+        // THIS IS THE ONLY TIME we do direct assignment
+        // to this.state
+        this.state = {lat: null, errorMessage: ''};
+
+        window.navigator.geolocation.getCurrentPosition(
+            (position) => {
+                // we called setstate!!!
+                this.setState({lat: position.coords.latitude});
+
+                // you don't want to do --> this.state.lat = position.coords.latitude
+            },
+            (err) => {
+                this.setState({errorMessage: err.message})
+            }
+        );
+    }
+
+    // React says we have to define render!!
+    render() {
+        // if there is an error message and no latitude show the following!
+        if (this.state.errorMessage && !this.state.lat) {
+            return <div>Error: {this.state.errorMessage}</div>
+        }
+
+        // if there is no error message and a latitude show the following!
+        if(!this.state.errorMessage && this.state.lat) {
+            return <div>Latitude: {this.state.lat}</div>
+        }
+
+        return <div>Loading...</div>;
+    }
 }
